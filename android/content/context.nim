594201cc2decc946140e75608e40017200fff216
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2017-10-18 16:14:41 +0300

    Added getCacheDir

--- android/content/context.nim
+++ android/content/context.nim
@@ -2,8 +2,13 @@ import jnim
 import android.content.content_resolver
 import android.content.res.asset_manager
 
+jclass java.io.File* of JVMObject: # TODO: This should be defined elsewhere
+    proc getAbsolutePath*: string
+
 jclass android.content.Context* of JVMObject:
     proc INPUT_METHOD_SERVICE*: string {.prop, `static`, final.}
     proc getSystemService*(name: string): JVMObject
     proc getAssets*: AssetManager
     proc getContentResolver*: ContentResolver
+    proc getCacheDir*: File
+    proc getExternalCacheDir*: File
