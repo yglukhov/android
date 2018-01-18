d87c26d09caec2f92950883235ee6c7beaf1a31b
Ievgen Svietikov <forlan@zeoalliance.com>
2018-01-18 16:42:54 +0200

    Added android.net.uri and a few Intent methods

--- android/content/intent.nim
+++ android/content/intent.nim
@@ -1,5 +1,10 @@
 import jnim
+import java.lang
 import android.os.bundle
+import android.net.uri
 
 jclass android.content.Intent* of JVMObject:
     proc getExtras*(): Bundle
+    proc getData*(): Uri
+    proc hasExtra*(key: string): bool
+    proc getStringExtra*(key: string): String
---
+++ android/net/uri.nim
@@ -0,0 +1,7 @@
+import jnim, java.lang
+
+jclass android.net.Uri* of JVMObject:
+    proc getPath*(): String
+    proc getHost*(): String
+    proc getScheme*(): String
+    proc toString*(): String
