779333417afb0875fddbe26bcccf03006930e5ad
Vladimir Melnichuk <tormund@zeo.lcl>
2018-03-13 16:59:39 +0200

    extend bundle bindings

--- android/os/base_bundle.nim
+++ android/os/base_bundle.nim
@@ -1,4 +1,16 @@
 import jnim
 
 jclass android.os.BaseBundle* of JVMObject:
+    proc clear*()
+    proc containsKey*(key: string): bool
+    proc isEmpty*(): bool
+    
     proc getString*(key: string): string
+
+    proc putBoolean*(key: string, val: bool)
+    proc putDouble*(key: string, val: jdouble)
+    proc putInt*(key: string, val: int)
+    proc putLong*(key: string, val: jlong)
+    proc putString*(key: string, val: string)
+    
+     
--- android/os/bundle.nim
+++ android/os/bundle.nim
@@ -1,4 +1,4 @@
 import jnim
 import android.os.base_bundle
-
+export base_bundle
 jclassDef android.os.Bundle* of BaseBundle
